e5be8ff8e35a7c7d17b51223624f7d32e9cd9ce5
SaraiMancia <manciabrego@gmail.com>
2022-05-29 17:07:30 -0600

    insertar y consultar

--- src/java/controladores/recibir_SA.java
+++ src/java/controladores/recibir_SA.java
@@ -7,19 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.DatosPersona;
 
 
 public class recibir_SA extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -37,40 +30,36 @@ public class recibir_SA extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
+            String dui=request.getParameter("txtDui");
+        String apellidos=request.getParameter("txtApellido");
+        String nombres=request.getParameter("txtNombre");
+        
+        DatosPersona person= new DatosPersona();
+        person.setDui(dui);
+        person.setApellido(apellidos);
+        person.setNombre(nombres);
+        
+        if(person.InsertarDatos()==true){
+            request.getRequestDispatcher("exito.jsp").forward(request, response);
+        }else{
+          request.getRequestDispatcher("noexito.jsp").forward(request, response);   
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+   
     @Override
     public String getServletInfo() {
         return "Short description";
--- src/java/modelo/DatosPersona.java
+++ src/java/modelo/DatosPersona.java
@@ -56,5 +56,33 @@ public class DatosPersona {
         this.apellido = apellido;
         this.nombre = nombre;
     }
-       
+           public boolean InsertarDatos(){
+            try{
+                String miQuery="insert into tb_persona values('" + dui + "','" + apellido + "','" + nombre + "');";
+                int es=0;
+                state=cnn.createStatement();
+                es=state.executeUpdate(miQuery);
+                if (es==1){
+                    return true;
+                }
+            } catch (SQLException ex){
+                        Logger.getLogger(DatosPersona.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                return false;
+            }
+            public ArrayList<DatosPersona>ConsultarR(){
+            ArrayList<DatosPersona>person=new ArrayList();
+            try{
+                String miQuery="select*from tb_persona;";
+                state=cnn.createStatement();
+                result=state.executeQuery(miQuery);
+                while(result.next()){
+                    
+                    person.add(new DatosPersona(result.getString("dui_persona"),result.getString("apellidos_persona"),result.getString("nombre_persona")));
+                }
+                }catch (SQLException ex){
+                          Logger.getLogger(DatosPersona.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            return person;
+}
 }
