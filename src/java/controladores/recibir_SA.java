58af949cf4843f9a8e65facdca2231be62db33dd
SaraiMancia <manciabrego@gmail.com>
2022-05-29 18:00:34 -0600

    -----

--- src/java/controladores/recibir_SA.java
+++ src/java/controladores/recibir_SA.java
@@ -15,19 +15,8 @@ public class recibir_SA extends HttpServlet {
    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet recibir_SA</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet recibir_SA at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+      
+       
     }
 
   
